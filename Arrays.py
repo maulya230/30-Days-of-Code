263e37a342d737c369f22662531b7c245a0cddbd
maulya230 <75597705+maulya230@users.noreply.github.com>
2021-12-29 19:16:33 +0530

    Create Arrays.py

---
+++ Arrays.py
@@ -0,0 +1,17 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+    i=len(arr)-1
+    while i>=0:
+        print(arr[i],end=" ")
+        i-=1
